d78b2fce5dcd160f1adc1285c5b1ecb4a6447763
Anthony Truby <atruby4@icloud.com>
2026-05-21 20:01:49 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -6,67 +6,69 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"slices"
 	"strings"
 )
 
-var shellBuiltins = []string{
-	"echo", "type", "exit", "pwd", "cd",
+type builtinFunc func([]string)
+
+var builtins map[string]builtinFunc
+
+func init() {
+	builtins = map[string]builtinFunc{
+		"echo": handleEcho,
+		"type": handleType,
+		"exit": handleExit,
+		"pwd":  handlePWD,
+		"cd":   handleCD,
+	}
 }
 
 func main() {
 
 	for {
-		cmd, err := readCommand()
+		input, err := readInput()
 		if err != nil {
 			fmt.Println(err.Error())
 			continue
 		}
 
-		if cmd == "exit" {
-			break
-		}
+		cmd, after := splitInput(input)
 
-		if cmd == "pwd" {
-			handlePWD()
-			continue
-		}
-
-		after, found := strings.CutPrefix(cmd, "cd")
-		if found {
-			handleCD(strings.TrimSpace(after))
-			continue
-		}
-
-		after, found = strings.CutPrefix(cmd, "echo ")
-		if found {
-			fmt.Println(after)
-			continue
-		}
-
-		after, found = strings.CutPrefix(cmd, "type ")
-		if found {
-			handleType(after)
-			continue
-		}
-
-		split_cmd := strings.Fields(cmd)
-		_, err = exec.LookPath(split_cmd[0])
+		args, err := lex(after)
 		if err != nil {
-			fmt.Printf("%s: command not found\n", split_cmd[0])
+			fmt.Printf("err: %s", err.Error())
 			continue
 		}
 
-		out, err := exec.Command(split_cmd[0], split_cmd[1:]...).Output()
+		if fn, exists := builtins[cmd]; exists {
+			fn(args)
+			continue
+		}
+
+		_, err = exec.LookPath(cmd)
+		if err != nil {
+			fmt.Printf("%s: command not found\n", cmd)
+			continue
+		}
+
+		err = executePathCommand(cmd, args)
 		if err != nil {
 			fmt.Println(err.Error())
-			continue
 		}
-		fmt.Print(string(out))
 	}
 }
 
-func readCommand() (string, error) {
+func executePathCommand(cmd string, args []string) error {
+	out, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		return fmt.Errorf("cmd execution error: %v", err)
+
+	}
+	fmt.Print(string(out))
+	return nil
+}
+
+func readInput() (string, error) {
 	fmt.Print("$ ")
 	reader := bufio.NewReader(os.Stdin)
 	line, err := reader.ReadString('\n')
@@ -77,21 +79,37 @@ func readCommand() (string, error) {
 	return line, nil
 }
 
-func handleType(after string) {
-	if slices.Contains(shellBuiltins, after) {
-		fmt.Printf("%s is a shell builtin\n", after)
-		return
-	}
-
-	s, err := exec.LookPath(after)
-	if err != nil {
-		fmt.Printf("%s: not found\n", after)
-		return
-	}
-	fmt.Printf("%s is %s\n", after, s)
+func splitInput(input string) (cmd string, after string) {
+	cmd = strings.Fields(input)[0]
+	after = strings.TrimPrefix(input, cmd)
+	return cmd, strings.TrimSpace(after)
 }
 
-func handlePWD() {
+func handleExit(_ []string) {
+	os.Exit(0)
+}
+
+func handleEcho(args []string) {
+	fmt.Println(strings.Join(args, " "))
+}
+
+func handleType(args []string) {
+	for _, arg := range args {
+		if _, exists := builtins[arg]; exists {
+			fmt.Printf("%s is a shell builtin\n", arg)
+			return
+		}
+
+		s, err := exec.LookPath(arg)
+		if err != nil {
+			fmt.Printf("%s: not found\n", arg)
+			return
+		}
+		fmt.Printf("%s is %s\n", arg, s)
+	}
+}
+
+func handlePWD(_ []string) {
 	path, err := os.Getwd()
 	if err != nil {
 		log.Println(err)
@@ -99,7 +117,14 @@ func handlePWD() {
 	fmt.Println(path)
 }
 
-func handleCD(arg string) {
+func handleCD(args []string) {
+	if len(args) > 1 {
+		fmt.Printf("cd: too many arguments")
+		return
+	}
+
+	arg := args[0]
+
 	if arg == "~" {
 		arg = os.Getenv("HOME")
 	}
@@ -107,4 +132,45 @@ func handleCD(arg string) {
 	if err != nil {
 		fmt.Printf("cd: %s: No such file or directory\n", arg)
 	}
+}
+
+func lex(after string) ([]string, error) {
+	var tokens []string
+	var current []rune
+
+	inSingleQuote := false
+
+	for _, ch := range after {
+		switch ch {
+
+		// toggle single quote tracker
+		case '\'':
+			inSingleQuote = !inSingleQuote
+
+		case ' ', '\t', '\n':
+			if inSingleQuote {
+				// spaces are part of the token
+				current = append(current, ch)
+			} else {
+				// spaces outside quotes means to split the token
+				if len(current) > 0 {
+					tokens = append(tokens, string(current))
+					current = nil
+				}
+			}
+
+		default:
+			current = append(current, ch)
+		}
+	}
+
+	if inSingleQuote {
+		return nil, fmt.Errorf("unterminated single quote")
+	}
+
+	if len(current) > 0 {
+		tokens = append(tokens, string(current))
+	}
+
+	return tokens, nil
 }
